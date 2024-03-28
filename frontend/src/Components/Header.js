5524f8620daa8f8c400fbc110b0e48d45dd070fd
Ciepol <adamulicho@gmail.com>
2024-03-28 14:31:30 +0100

    frontend - userForm init

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="App-Header">
+      <div>Find the best movie for today!</div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ frontend/src/Components/UserForm.js
@@ -0,0 +1,94 @@
+import { useState } from "react";
+
+const UserForm = () => {
+  const [userMood, setUserMood] = useState("Happy");
+  const [userGenre, setUserGenre] = useState("Drama");
+  const [userWorkingDay, setuserWorkingDay] = useState(false);
+
+  function sendUserDataHandler(userData){
+    fetch('http://localhost:8080/receive-json',{
+      method:'POST',
+      body:JSON.stringify(userData),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const userData = {
+      mood: userMood,
+      genre: userGenre,
+      workingDay: userWorkingDay,
+    };
+    sendUserDataHandler(userData);
+  }
+
+  return (
+    <form className="UserForm" onSubmit={submitHandler}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <label for="mood">How are you today?</label>
+        <select
+          id="mood"
+          name="mood"
+          onChange={(e) => {
+            setUserMood(e.target.value);
+          }}
+          style={{ width: "5rem" }}
+        >
+          <option value="Happy">Happy</option>
+          <option value="Sad">Sad</option>
+          <option value="Neutral">Neutral</option>
+        </select>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          gap: "12px",
+          justifyContent: "space-between",
+        }}
+      >
+        <label for="genre">What genre do you like?</label>
+        <select
+          id="genre"
+          name="genre"
+          onChange={(e) => {
+            setUserGenre(e.target.value);
+          }}
+          style={{ width: "5rem" }}
+        >
+          <option value="Drama">Drama</option>
+          <option value="Comedy">Comedy</option>
+          <option value="Action">Action</option>
+        </select>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          gap: "12px",
+          justifyContent: "space-between",
+        }}
+      >
+        <label for="workingDay">Did you work today?</label>
+        <div>
+          <input
+            type="checkbox"
+            id="workingDay"
+            name="workingDay"
+            className="checkbox"
+            style={{ marginRight: "4px" }}
+            onChange={(e) => {
+              setuserWorkingDay(e.target.checked);
+            }}
+          />
+          <label for="workingDay">Worked</label>
+        </div>
+      </div>
+      <button className="button-submit-UserForm">Find movie</button>
+    </form>
+  );
+};
+export default UserForm;
