3e8e354c2df470753bd8a6b0608f59ade2eeb795
CiepLap <adamulicho@gmail.com>
2024-06-12 01:18:56 +0200

    Added movie description logic and component after movie is founded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import "./App.css";
+import React from "react";
+import { useState } from "react";
 import Header from "./Components/Header";
 import UserForm from "./Components/UserForm";
+import MovieDescription from "./Components/MovieDescription";
+import "./App.css";
 
 function App() {
+  const [movieIsFound, setMovieIsFound] = useState(false)
+  const [movieData, setMovieData] = useState({name:'', description:''})
   return (
     <div className="App">
       <Header />
-      <UserForm />
+      <UserForm setMovieIsFound={setMovieIsFound} setMovieData={setMovieData} />
+      {movieIsFound && <MovieDescription movieData={movieData}/>}
     </div>
   );
 }
