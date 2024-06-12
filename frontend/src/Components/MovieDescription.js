2b9d74765792bd5c1ad9b4032b8732c3c3282ad2
CiepLap <adamulicho@gmail.com>
2024-06-12 13:18:41 +0200

    added style to movie description

--- frontend/src/Components/MovieDescription.js
+++ frontend/src/Components/MovieDescription.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function MovieDescription({movieData}) {
     return(<div className="UserForm">
-    <div>{movieData.name}</div>
+    <div style={{ textAlign: "center",backgroundColor: "white", borderRadius: "10px", border: "2px solid #61dafb", maxWidth: "600px" }}>{movieData.name}</div>
     <div>{movieData.description}</div>
     </div>);
-}
+}   
