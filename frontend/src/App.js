843101d50f7fecb16bdef7cff87c4f7bfb725318
CiepLap <adamulicho@gmail.com>
2024-06-16 00:50:09 +0200

    FE: added isLoading functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,17 @@ import Header from "./Components/Header";
 import UserForm from "./Components/UserForm";
 import MovieDescription from "./Components/MovieDescription";
 import "./App.css";
+import LoadingIcon from "./Components/LoadingIcon";
 
 function App() {
   const [movieIsFound, setMovieIsFound] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const [movieData, setMovieData] = useState({name:'', description:''})
   return (
     <div className="App">
       <Header />
-      <UserForm setMovieIsFound={setMovieIsFound} setMovieData={setMovieData} />
+      <UserForm setMovieIsFound={setMovieIsFound} setMovieData={setMovieData} isLoading={isLoading} setIsLoading={setIsLoading} />
+      {isLoading && <LoadingIcon/>}
       {movieIsFound && <MovieDescription movieData={movieData}/>}
     </div>
   );
